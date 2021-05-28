c430b481cda683af47bf9fb69c0ed8cdf8396ada
h-yerim <hrim1103@gmail.com>
2021-05-28 17:47:57 +0900

    2021-05-28-v3

--- src/java_20210510/AdminDemo.java
+++ src/java_20210510/AdminDemo.java
@@ -2,11 +2,9 @@ package java_20210510;
 
 public class AdminDemo {
 	public static void main(String[] args) {
-		//new 뒤의 admin은 매개변수 없는 생성자를 호출
 		//같은 클래스 내 객체 생성 (생성자의 접근한정자가 public일 경우)
 		//생성자는 보통 private or public 이다.
 		
-		//Admin a <-역시 클래스의 접근한정자에 의해 선언이 가능함. 띠라서 class가 public이 아닐경우 다른패키지일때 에러가 날 수 있다.
 		Admin a = new Admin("hhh","123","aa@aa",20);
 		
 		System.out.println(a.getId());
--- src/java_20210510/aa/AdminDemo.java
+++ src/java_20210510/aa/AdminDemo.java
@@ -5,6 +5,8 @@ public class AdminDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//new 뒤의 admin은 매개변수 없는 생성자를 호출
+		//Admin a <-역시 클래스의 접근한정자에 의해 선언이 가능함. 띠라서 class가 public이 아닐경우 다른패키지일때 에러가 날 수 있다.
 		Admin a = new Admin();
 	}
 
